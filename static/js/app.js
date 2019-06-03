65b6b9b2b20c488bdd135030cb5c58447719ace9
Esjael <stewartjlapan@gmail.com>
2019-06-03 11:46:46 -0600

    Wrapped table render in an if else statement to try to get around the error generated by the null user input, still getting a null value error, though

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,56 @@
 var tbody = d3.select("tbody");
 
-// Append the table data from the data file
-data.forEach(datum => {
-    var tr = tbody.append("tr");
-    Object
-        .entries(datum)
-        .forEach(([datumKey, datumValue]) => {
-            tr.append("td").text(datumValue)
-        })
-})
-
 // Provide a means of filtering the table by date
+
+var inputSel = d3.select("form-control");
+var submit = d3.select("filter-btn");
+
+
+// Append the table data from the data file
+// if there is no user input
+if (inputSel.property("value") == null) {
+    data.forEach(datum => {
+        var tr = tbody.append("tr");
+        Object
+            .entries(datum)
+            .forEach(([datumKey, datumValue]) => {
+                tr.append("td").text(datumValue)
+            })
+    });
+} else {
+    submit.on("click", function () {
+        // Prevent the page from refreshing
+        d3.event.preventDefault();
+        var matchingDates = data.filter(sighting => {
+            return sighting.Date == inputValue;
+            console.log(matchingDates)
+            matchingDates.forEach(datum => {
+                var tr = tbody.append("tr");
+                Object
+                    .entries(datum)
+                    .forEach(([datumKey, datumValue]) => {
+                        tr.append("td").text(datumValue)
+                    })
+            });
+        });
+    });
+};
+
+
+
+
+
+// // Get value of user input if it exists
+// if (inputSel.property("value") != null) {
+//     var inputValue = inputSel.property("value");
+
+// // Filter the data using the input
+
+//     var matchingDates = data.filter(sighting => {
+//         return sighting.Date == inputValue;
+//         console.log(matchingDates)
+// });
+// } else {
+//     console.log("There is no user input...")
+// }
+// })
