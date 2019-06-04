27c11447cd72418c692623283f34005cf7356e94
Esjael <stewartjlapan@gmail.com>
2019-06-03 18:51:35 -0600

    Changed some stuff

--- static/js/app.js
+++ static/js/app.js
@@ -7,9 +7,27 @@ var submit = d3.select("filter-btn");
 
 
 // Append the table data from the data file
-// if there is no user input
-if (inputSel.property("value") == null) {
-    data.forEach(datum => {
+data.forEach(datum => {
+    var tr = tbody.append("tr");
+    Object
+        .entries(datum)
+        .forEach(([datumKey, datumValue]) => {
+            tr.append("td").text(datumValue)
+        })
+});
+// The code above this point works. Hooray!
+
+
+// After 
+submit.on("click", function () {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    var inputValue = inputSel.property("value");
+    var matchingDates = data.filter(sighting => {
+        return sighting.Date == inputValue;
+    });
+    console.log(matchingDates)
+    matchingDates.forEach(datum => {
         var tr = tbody.append("tr");
         Object
             .entries(datum)
@@ -17,42 +35,7 @@ if (inputSel.property("value") == null) {
                 tr.append("td").text(datumValue)
             })
     });
-} else {
-    // After 
-    submit.on("click", function () {
-        // Prevent the page from refreshing
-        d3.event.preventDefault();
-        var inputValue = inputSel.property("value");
-        var matchingDates = data.filter(sighting => {
-            return sighting.Date == inputValue;
-            console.log(matchingDates)
-            matchingDates.forEach(datum => {
-                var tr = tbody.append("tr");
-                Object
-                    .entries(datum)
-                    .forEach(([datumKey, datumValue]) => {
-                        tr.append("td").text(datumValue)
-                    })
-            });
-        });
-    });
-};
+
+});
 
 
-
-
-
-// // Get value of user input if it exists
-// if (inputSel.property("value") != null) {
-//     var inputValue = inputSel.property("value");
-
-// // Filter the data using the input
-
-//     var matchingDates = data.filter(sighting => {
-//         return sighting.Date == inputValue;
-//         console.log(matchingDates)
-// });
-// } else {
-//     console.log("There is no user input...")
-// }
-// })
